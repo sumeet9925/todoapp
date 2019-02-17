c727184ea44f684178af9d964f1460aeedd9d92c
sumeet <sumeet@uxgorilla.com>
2019-02-18 00:40:18 +0530

    adding delete function and styling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,12 +7,18 @@ app.controller('mainCtrlr', ['$scope','addService', function ($scope,addService)
 
 	scope.addItem = function(){
 		if(scope.item.name.length > 0){
-			scope.list = addService.add({'name':scope.item.name, 'done':false});
+			scope.list = addService.add({'name':scope.item.name});
 			scope.item.name = '';
 		}else{
 			alert("Please enter a value");
 		}
 	}
+	scope.delete = function(idx){
+		addService.delete(idx);
+	}
+	// scope.update = function(idx){
+	// 	addService.update(idx);
+	// }
 }])
 
 app.service('addService', [function () {
@@ -22,4 +28,12 @@ app.service('addService', [function () {
 		  list.push(item)
 		  return list;
 	}
+
+	this.delete = function(idx){
+		scope.list.splice(idx,1);
+	}
+
+	// this.update = function(idx){
+	// 	scope.item.name = scope.list[idx].name;
+	// }
 }])
