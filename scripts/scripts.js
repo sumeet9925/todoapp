14bd72d57de3d60195497855aaf4b5a9eb2ecdf9
sumeet <sumeet@uxgorilla.com>
2019-03-03 16:09:43 +0530

    added localStorage but update function is not working for now

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,41 +3,74 @@ var app = angular.module("todo", []);
 app.controller('mainCtrlr', ['$scope','addService', function ($scope,addService) {
 	var scope = $scope;
 	window.scope = scope;
-	scope.list = [];
+	
+	if(!localStorage.getItem("data")){
+		localStorage.setItem("data", "[]");
+	}else{
+		var data = localStorage.getItem("data");
+		scope.data = JSON.parse(data);
+	}
 
+	
 	scope.addItem = function(){
 		if(scope.item.name.length > 0){
 			scope.list = addService.add({'name':scope.item.name, 'update':false});
+			addService.localStorageUpdate();
 			scope.item.name = '';
 		}else{
 			alert("Please enter a value");
 		}
+		
+		
 	}
 	scope.delete = function(idx){
 		addService.delete(idx);
+		addService.localStorageUpdate();
 	}
 	scope.edit = function(idx){
-		scope.list[idx].update = true;
+		addService.edit(idx);
+		addService.localStorageUpdate();
 	}
 	scope.update = function(idx){
 		addService.update(idx);
+		addService.localStorageUpdate();
 	}
+
+	scope.$watch(function(){
+		return localStorage.getItem('data');
+	}, function(newData, oldCodes){
+		scope.data = JSON.parse(newData);
+	});
+
 }])
 
 app.service('addService', [function () {
-	var list = [];
+	
+	var _list = localStorage.getItem("data");
+	
+	var list = JSON.parse(_list) || [];
 
 	this.add = function(item){
-		  list.push(item)
-		  return list;
+		list.push(item)
+		return list;
 	}
 
 	this.delete = function(idx){
-		scope.list.splice(idx,1);
+		list.splice(idx,1);
+		return list;
+
+	}
+	this.edit = function(idx){
+		list[idx].update = true;
+		return list;
+	}
+	this.update = function(idx){
+		list[idx].name = scope.item.name;
+		list[idx].update = false;
+		return list;
 	}
 
-	this.update = function(idx){
-		scope.list[idx].name = scope.list[idx].name;
-		scope.list[idx].update = false;
+	this.localStorageUpdate = function(){
+		localStorage.setItem("data", JSON.stringify(scope.list))	
 	}
 }])
